57d0ca8701cbdac87ecc61c749223f880214b843
Rerumu <25379555+Rerumu@users.noreply.github.com>
2024-12-03 16:22:18 -0500

    Add instantiation macros

--- src/fixed/mod.rs
+++ src/fixed/mod.rs
@@ -6,3 +6,20 @@ mod into_iter;
 
 pub use collection::Fixed;
 pub use into_iter::IntoIter;
+
+#[macro_export]
+macro_rules! fixed {
+	() => (
+		$crate::fixed::Fixed::new()
+	);
+	($element:expr; $count:expr) => ({
+		let iter = ::core::iter::repeat_n($element, $count);
+
+		$crate::fixed::Fixed::from_iter(iter)
+	});
+	($($element:expr),+ $(,)?) => ({
+		let array = [$($element),+];
+
+		$crate::fixed::Fixed::from_iter(array)
+	});
+}
--- src/resizable/mod.rs
+++ src/resizable/mod.rs
@@ -6,3 +6,20 @@ mod into_iter;
 
 pub use collection::Resizable;
 pub use into_iter::IntoIter;
+
+#[macro_export]
+macro_rules! resizable {
+	() => (
+		$crate::resizable::Resizable::new()
+	);
+	($element:expr; $count:expr) => ({
+		let iter = ::core::iter::repeat_n($element, $count);
+
+		$crate::resizable::Resizable::from_iter(iter)
+	});
+	($($element:expr),+ $(,)?) => ({
+		let iter = [$($element),+];
+
+		$crate::resizable::Resizable::from_iter(iter)
+	});
+}
